adfe50a56de792d3ab75f040675afe407646f4a5
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-03-09 00:50:53 -0400

    More tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -7,4 +7,30 @@ RSpec.describe Voting::Question do
 
   it { expect(subject).to have_many(:answers) }
   it { expect(subject).to have_many(:votes) }
+
+  let(:inst) { Fabricate(:question) }
+
+  context '#closed?' do
+    it 'should return true when the voting is closed' do
+      inst.closed = true
+      expect(inst).to be_closed
+    end
+
+    it 'should return false when the voting is open' do
+      inst.closed = false
+      expect(inst).to_not be_closed
+    end
+  end
+
+  context '#close_voting!' do
+    it 'should mark the voting as closed' do
+      inst.closed = false
+      expect { inst.close_voting! }.to change { inst.closed? }.from(false).to(true)
+    end
+
+    it 'should not affect closed questions' do
+      inst.close_voting!
+      expect { inst.close_voting! }.to_not change { inst.closed? }
+    end
+  end
 end
