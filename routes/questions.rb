394509b978dd160bea80a159d9a6ac8389974d02
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-03-10 00:39:25 -0400

    Allow customizing number of questions

--- routes/questions.rb
+++ routes/questions.rb
@@ -7,8 +7,8 @@ class Voting::App
 
   get '/questions/new/?', :auth => nil do
     question = Voting::Question.new({
-      answers: 3.times.map { Voting::Answer.new },
-      voters: 3.times.map { Voting::Voter.new }
+      answers: (params[:answer_count] || 3).times.map { Voting::Answer.new },
+      voters: (params[:voter_count] || 3).times.map { Voting::Voter.new }
     })
     erb :'questions/new', :locals => {question: question}
   end
@@ -19,8 +19,8 @@ class Voting::App
     voter_attributes = question.delete('voter_attributes') || []
 
     question = Voting::Question.new(question)
-    question.answers = answer_attributes.map { |a| Voting::Answer.new(a) }
-    question.voters = voter_attributes.map { |v| Voting::Voter.new(v) }
+    question.answers = answer_attributes.select { |a| a.nil? || a.empty? }.map { |a| Voting::Answer.new(a) }
+    question.voters = voter_attributes.select { |a| a.nil? || a.empty? }.map { |v| Voting::Voter.new(v) }
 
     if question.save
       flash[:success] = "You've successfully created a <a href='/questions/#{question.id}'>new question</a>."
