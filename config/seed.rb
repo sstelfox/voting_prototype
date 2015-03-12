e1fc0759fdb8a5ac64a3a27642058f4b85540b67
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-03-11 23:44:51 -0400

    Cleaned up seed file

--- config/seed.rb
+++ config/seed.rb
@@ -1,23 +1,42 @@
-def simple_log(msg)
-  printf("%-50.50s", msg)
+require 'securerandom'
+
+module SeedHelpers
+  def event(msg)
+    printf("%-50.50s", msg)
+  end
+
+  def event_failed
+    puts "[ Failed ]"
+  end
+
+  def event_success
+    puts "[ Done ]"
+  end
 end
 
-def failed
-  puts "[ Failed ]"
+module UserCreator
+  extend SeedHelpers
+
+  DEFAULT_USER_NAME = 'admin'
+  PASS_BYTE_STRENGH = 5
+
+  def self.new_user
+    Voting::User.new(
+      username: DEFAULT_USER_NAME,
+      password: password,
+      password_confirmation: password
+    )
+  end
+
+  def self.password
+    @password ||= SecureRandom.hex(PASS_BYTE_STRENGH)
+  end
+
+  def self.run
+    return if Voting::User.first(username: DEFAULT_USER_NAME)
+    event('Creating default user (%s:%s)...' % [DEFAULT_USER_NAME, password])
+    new_user.save ? event_success : event_failed
+  end
 end
 
-def success
-  puts "[ Done ]"
-end
-
-def skipped
-  puts "[ Skipped ]"
-end
-
-simple_log("Creating admin user...")
-if Voting::User.first(username: 'admin')
-  skipped
-else
-  u = Voting::User.new(username: 'admin', password: 'password', password_confirmation: 'password')
-  u.save ? success : failed
-end
+UserCreator.run
