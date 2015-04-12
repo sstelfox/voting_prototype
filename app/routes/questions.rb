7fe764a07aa0c5c3f143e88d0f038c48b14d2bff
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-04-12 19:01:22 -0400

    Fixed up question show view

--- app/routes/questions.rb
+++ app/routes/questions.rb
@@ -33,6 +33,54 @@ module Voting
       erb :'questions/show', locals: { question: question }
     end
 
+    post '/questions/:id/new_answer/?' do
+      halt(404) unless (question = Question.get(params[:id]))
+
+      text = params[:answer][:text]
+
+      if question.released?
+        flash[:alert] = 'Answers can only be added to unreleased questions.'
+        redirect "/questions/#{question.id}/"
+      end
+
+      if question.answers.all(text: text).any?
+        flash[:alert] = "Answers already exists for this question with that text '#{text}'."
+        redirect "/questions/#{question.id}/"
+      end
+
+      if Answer.create(question: question, text: text)
+        flash[:success] = "Answer successfully added with text #{text}."
+      else
+        flash[:alert] = "Failed to add answer with text #{text}."
+      end
+
+      redirect "/questions/#{question.id}/"
+    end
+
+    post '/questions/:id/new_voter/?' do
+      halt(404) unless (question = Question.get(params[:id]))
+
+      email = params[:voter][:email]
+
+      if question.released?
+        flash[:alert] = 'Voters can only be added to unreleased questions.'
+        redirect "/questions/#{question.id}/"
+      end
+
+      if question.voters.all(email: email).any?
+        flash[:alert] = "Voter already exists for this question with email '#{email}'."
+        redirect "/questions/#{question.id}/"
+      end
+
+      if Voter.create(question: question, email: email)
+        flash[:success] = "Voter successfully added with email #{email}."
+      else
+        flash[:alert] = "Failed to added voter with email #{email}."
+      end
+
+      redirect "/questions/#{question.id}/"
+    end
+
     get '/questions/:id/close/?', auth: nil do
       halt(404) unless (question = Question.get(params[:id]))
 
@@ -56,6 +104,17 @@ module Voting
 
     post '/questions/:question_id/vote/:token/?' do
       halt(404) unless (question = Question.get(params[:question_id]))
+
+      unless question.released?
+        flash[:alert] = 'No votes are allowed until the question has been released!'
+        redirect "/questions/#{question.id}/"
+      end
+
+      if question.closed?
+        flash[:alert] = 'No votes are allowed after the question has closed!'
+        redirect "/questions/#{question.id}/"
+      end
+
       halt(403) unless (voter = question.voters.all(token: params[:token]).first)
 
       if voter.answer!(params[:answer_ids].map(&:to_i))
